afdf8fff002964da2c6d0169b95484de4054c7be
cuypersbrent2007@gmail.com <cuypersbrent2007@gmail.com>
2022-02-05 20:07:06 +0100

    updated ownership checking

--- js/app.js
+++ js/app.js
@@ -78,15 +78,15 @@ function updateStatusText(isOwner, checking) {
   const statusText = document.querySelector('.owner-status');
   if(checking) {
     if(isOwner) {
-      statusText.innerText = `You do own ${COLLECTION_NAME}!! 😻 Let's see how many${renderDots(dots)}`;
+      statusText.innerText = `You do own ${Crazy Penguin's}! 🐧 Let's see how many${renderDots(dots)}`;
     } else {
-      statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME} 😻${renderDots(dots)}`;
+      statusText.innerText = `Checking to see if you own any ${Crazy Penguin's} 🐧${renderDots(dots)}`;
     }
   } else {
     if(isOwner) {
-      statusText.innerText = `You own ${editions.length} ${COLLECTION_NAME}!! 😻`;
+      statusText.innerText = `You own ${editions.length} ${Crazy Penguin's}! 🐧`;
     } else {
-      statusText.innerText = `You don't own any ${COLLECTION_NAME} 😿`;
+      statusText.innerText = `You don't own any ${Crazy Penguin's} 😭`;
     }
   }
   dots = dots === 3 ? 1 : dots + 1;
